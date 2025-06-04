cace3b78d8e3c364c08bbadb7cd48809c4e869a9
박재복 <pjb24@daum.net>
2025-06-04 21:34:46 +0900

    스크립트에 주석 추가

--- Assets/Scripts/CardFlip.cs
+++ Assets/Scripts/CardFlip.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class CardFlip : MonoBehaviour
 {
+    // Flip 방향 enum
     public enum FlipDirection
     {
         Left,
@@ -14,17 +15,22 @@ public class CardFlip : MonoBehaviour
         Bottom,
     };
 
+    // Flip을 할 방향 지정
     public FlipDirection flipDirection = FlipDirection.Left;
+    // Flip에 소요될 시간
     public float duration = 0.5f;
 
+    // Inspector에서 테스트를 수행할 수 있음.
     [ContextMenu("Test Flip Function")]
     private void TestFlipFunction()
     {
         Flip(flipDirection);
     }
 
+    // Flip 기능 구현 Right 방향 Flip이 기본 설정.
     private void Flip(FlipDirection direction = FlipDirection.Right)
     {
+        // 현재 회전 쿼터니언
         Quaternion currentRotation = transform.rotation;
 
         Vector3 angle3 = new Vector3(0, 0, 0);
@@ -45,10 +51,13 @@ public class CardFlip : MonoBehaviour
                 break;
         }
 
+        // 추가 회전 쿼터니언
         Quaternion rotationDelta = Quaternion.Euler(angle3);
 
+        // 목표 회전 = 현재 회전 * 추가 회전
         Quaternion targetRotation = currentRotation * rotationDelta;
 
+        // DOTween으로 duration 동안 회전 수행.
         transform.DORotateQuaternion(targetRotation, duration).SetEase(Ease.InOutQuad);
     }
 }
