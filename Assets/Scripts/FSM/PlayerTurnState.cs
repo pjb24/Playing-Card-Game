1bdb3102ef907873d0ee84234350956cc9685a8e
박재복 <pjb24@daum.net>
2025-06-25 12:38:01 +0900

    PlayerTurnState 구현, Hit와 Stand만 구현하고 Split과 DoubleDown은 구현하지 않음

--- Assets/Scripts/FSM/PlayerTurnState.cs
+++ Assets/Scripts/FSM/PlayerTurnState.cs
@@ -1,19 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Rendering.ShadowCascadeGUI;
 
 public class PlayerTurnState : IGameState
 {
+    private Player currentPlayer;
+    private PlayerHand currentHand;
+
     public void Enter()
     {
-        GameManager.Instance.ChangeState(new DealerTurnState());
+        currentPlayer = GameManager.Instance.characterManager.GetNextActivePlayer();
+        if (currentPlayer == null)
+        {
+            // 모든 플레이어의 행동 완료 딜러 턴으로 전이
+            GameManager.Instance.ChangeState(new DealerTurnState());
+
+            return;
+        }
+
+        currentHand = currentPlayer.GetActiveHand();
+        if (currentHand == null)
+        {
+            // 현재 플레이어의 행동 완료 플래그 설정
+            GameManager.Instance.characterManager.MarkCurrentPlayerDone();
+            // 현재 플레이어의 모든 핸드에 대한 행동 완료, 다음 플레이어 턴으로 전이
+            GameManager.Instance.ChangeState(new PlayerTurnState());
+
+            return;
+        }
+
+        GameManager.Instance.uiManager.ChangeToPlayerActionPanel();
+
+        // Button subscribe function
+        GameManager.Instance.uiManager.button_Hit.clicked += HandleHit;
+        GameManager.Instance.uiManager.button_Stand.clicked += HandleStand;
+        GameManager.Instance.uiManager.button_Split.clicked += HandleSplit;
+        GameManager.Instance.uiManager.button_DoubleDown.clicked += HandleDoubleDown;
     }
 
     public void Exit()
     {
+        // Unsubscribe function
+        GameManager.Instance.uiManager.button_Hit.clicked -= HandleHit;
+        GameManager.Instance.uiManager.button_Stand.clicked -= HandleStand;
+        GameManager.Instance.uiManager.button_Split.clicked -= HandleSplit;
+        GameManager.Instance.uiManager.button_DoubleDown.clicked -= HandleDoubleDown;
     }
 
     public void Update()
     {
+    }
+
+    public void HandleHit()
+    {
+        Card card = GameManager.Instance.deckManager.DrawCard();
+        currentHand.AddCard(card);
+        GameManager.Instance.InstancingCardToPlayer(card, currentHand.Cards.Count);
+
+        if (currentHand.IsBust())
+        {
+            NextHand();
+        }
+    }
+
+    public void HandleStand()
+    {
+        NextHand();
+    }
+
+    public void HandleSplit()
+    {
+
+    }
+
+    public void HandleDoubleDown()
+    {
+
+    }
+
+    private void NextHand()
+    {
+        currentPlayer.Stand(currentHand);
+        GameManager.Instance.ChangeState(new PlayerTurnState());
     }
 }
