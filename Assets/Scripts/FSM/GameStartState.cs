60bc3d7b888acd2b764936e4dabe4845d8590c29
박재복 <pjb24@daum.net>
2025-06-25 13:19:28 +0900

    불필요한 테스트 함수 제거

--- Assets/Scripts/FSM/GameStartState.cs
+++ Assets/Scripts/FSM/GameStartState.cs
@@ -28,28 +28,11 @@ public class GameStartState : IGameState
             GameManager.Instance.uiManager.button_Join.visible = true;
             GameManager.Instance.uiManager.button_Join.clicked += HandleJoin;
         }
-
-
-        //GameManager.Instance.uiManager.button_Join.clicked += TestCardInstancingAndMove;
-        //GameManager.Instance.uiManager.button_BetReset.clicked += TestResetChips;
-        //GameManager.Instance.uiManager.button_Bet1.clicked += TestCreateChipType1;
-        //GameManager.Instance.uiManager.button_Bet2.clicked += TestCreateChipType2;
-        //GameManager.Instance.uiManager.button_Bet3.clicked += TestCreateChipType3;
-        //GameManager.Instance.uiManager.button_Bet4.clicked += TestCreateChipType4;
-        //GameManager.Instance.uiManager.button_BetMax.clicked += TestCreateChipType5;
     }
 
     public void Exit()
     {
         GameManager.Instance.uiManager.button_Join.clicked -= HandleJoin;
-
-        //GameManager.Instance.uiManager.button_Join.clicked -= TestCardInstancingAndMove;
-        //GameManager.Instance.uiManager.button_BetReset.clicked -= TestResetChips;
-        //GameManager.Instance.uiManager.button_Bet1.clicked -= TestCreateChipType1;
-        //GameManager.Instance.uiManager.button_Bet2.clicked -= TestCreateChipType2;
-        //GameManager.Instance.uiManager.button_Bet3.clicked -= TestCreateChipType3;
-        //GameManager.Instance.uiManager.button_Bet4.clicked -= TestCreateChipType4;
-        //GameManager.Instance.uiManager.button_BetMax.clicked -= TestCreateChipType5;
     }
 
     public void Update()
@@ -64,47 +47,6 @@ public class GameStartState : IGameState
 
         // Change state to BettingState
         GameManager.Instance.ChangeState(new BettingState());
-    }
-
-    private void TestCardInstancingAndMove()
-    {
-        GameManager.Instance.InstancingCardToPlayer(GameManager.Instance.deckManager.DrawCard(), null);
-    }
-
-    private void TestResetChips()
-    {
-        GameManager.Instance.chipFactory.ResetChips();
-    }
-
-    private void TestCreateChipType1()
-    {
-        GameManager.Instance.chipFactory.CreateChipType1();
-        GameManager.Instance.chipFactory.UpdateChipPosition();
-    }
-
-    private void TestCreateChipType2()
-    {
-        GameManager.Instance.chipFactory.CreateChipType2();
-        GameManager.Instance.chipFactory.UpdateChipPosition();
-    }
-
-    private void TestCreateChipType3()
-    {
-        GameManager.Instance.chipFactory.CreateChipType3();
-        GameManager.Instance.chipFactory.UpdateChipPosition();
-    }
-
-    private void TestCreateChipType4()
-    {
-        GameManager.Instance.chipFactory.CreateChipType4();
-        GameManager.Instance.chipFactory.UpdateChipPosition();
-    }
-
-    private void TestCreateChipType5()
-    {
-        GameManager.Instance.chipFactory.ResetChips();
-        GameManager.Instance.chipFactory.CreateChipType5();
-        GameManager.Instance.chipFactory.UpdateChipPosition();
     }
 
     private void UpdateUI_PlayerInfos()
